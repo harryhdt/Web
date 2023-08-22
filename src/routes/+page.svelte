01a2d0b6f88c88c9d7ad5ffb5e2d1146c8f8f6e4
Harry Hidayat <harryhdtofficial@gmail.com>
2023-08-22 09:18:48 +0800

    simple seo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Harry Hidayat</title>
+	<meta
+		name="description"
+		content="a Fullstack Software Engineer. I have been programming since 2020. Especially
+			on Web and Mobile Development"
+	/>
+</svelte:head>
