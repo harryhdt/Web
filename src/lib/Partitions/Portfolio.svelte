6436c6c25ec5ededca1fc63dc8ed2cbbd955b8ff
Harry Hidayat <harryhdtofficial@gmail.com>
2023-10-22 13:44:41 +0800

    add new portfolio

--- src/lib/Partitions/Portfolio.svelte
+++ src/lib/Partitions/Portfolio.svelte
@@ -11,10 +11,10 @@
 				/>
 				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
 			</div>
-			<div class="mt-2">
+			<div class="flex flex-col flex-grow mt-2">
 				<h5 class="font-semibold text-neutral-800">My To Do</h5>
-				<p class="text-sm">is a Simple To Do App, with support cross platform.</p>
-				<div class="grid grid-cols-2 mt-2 text-center gap-x-1">
+				<p class="mb-2 text-sm">is a Simple To Do App, with support cross platform.</p>
+				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
 					<a
 						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
 						target="_blank"
@@ -24,6 +24,36 @@
 						class="px-2.5 py-1.5 text-white transition-all duration-200 rounded bg-amber-600 hover:bg-amber-700/90 active:scale-95"
 						target="_blank"
 						href="https://github.com/harryhdt/My-To-Do">Source</a
+					>
+				</div>
+			</div>
+		</div>
+		<div class="flex flex-col border rounded bg-neutral-100 border-neutral-200 p-2.5">
+			<div
+				class="relative flex items-center justify-center w-full aspect-[3/2] bg-neutral-200 rounded overflow-hidden"
+			>
+				<img
+					src="https://cdn.harryhdt.dev/logo-link-preview.webp"
+					class="absolute object-contain w-full h-full scale-90"
+					alt="Link Preview"
+				/>
+				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
+			</div>
+			<div class="flex flex-col flex-grow mt-2">
+				<h5 class="font-semibold text-neutral-800">Live Preview</h5>
+				<p class="mb-2 text-sm">
+					Get link preview like title, description, favicon, thumbnail and etc.
+				</p>
+				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
+					<a
+						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
+						target="_blank"
+						href="https://link-preview.harryhdt.dev">Demo</a
+					>
+					<a
+						class="px-2.5 py-1.5 text-white transition-all duration-200 rounded bg-amber-600 hover:bg-amber-700/90 active:scale-95"
+						target="_blank"
+						href="https://github.com/harryhdt/LinkPreview">Source</a
 					>
 				</div>
 			</div>
