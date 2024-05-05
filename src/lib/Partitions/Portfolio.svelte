9be56556810c9791b7ab38fb307b021b7d7d302c
harryhdt <harryhdtofficial@gmail.com>
2024-05-05 12:01:45 +0800

    add new portofolio

--- src/lib/Partitions/Portfolio.svelte
+++ src/lib/Partitions/Portfolio.svelte
@@ -90,5 +90,35 @@
 				</div>
 			</div>
 		</div>
+		<div class="flex flex-col border rounded bg-neutral-100 border-neutral-200 p-2.5">
+			<div
+				class="relative flex items-center justify-center w-full aspect-[3/2] bg-neutral-200 rounded overflow-hidden"
+			>
+				<img
+					src="https://cdn.harryhdt.dev/logo-classrooms-color.webp"
+					class="absolute object-contain w-full h-full scale-90"
+					alt="Classrooms Color"
+				/>
+				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
+			</div>
+			<div class="flex flex-col flex-grow mt-2">
+				<h5 class="font-semibold text-neutral-800">Classrooms Color</h5>
+				<p class="mb-2 text-sm">
+					Buat kelas Anda yang penuh warna, nikmati suasana baru yang menyenangkan.
+				</p>
+				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
+					<a
+						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
+						target="_blank"
+						href="https://classroom-colors.harryhdt.dev">Demo</a
+					>
+					<a
+						class="px-2.5 py-1.5 text-white transition-all duration-200 rounded bg-amber-600 hover:bg-amber-700/90 active:scale-95"
+						target="_blank"
+						href="https://github.com/harryhdt/classroom-colors">Source</a
+					>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
