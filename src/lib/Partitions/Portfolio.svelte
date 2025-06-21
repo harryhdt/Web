c875f7319084f0de04a915676537a5c66b8f0b39
Harry H <harryhdtofficial@gmail.com>
2025-06-21 09:22:58 +0800

    feat: ookit portfolio and remove some

--- src/lib/Partitions/Portfolio.svelte
+++ src/lib/Partitions/Portfolio.svelte
@@ -1,111 +1,44 @@
-<div class="flex flex-col grow pb-10">
+<div class="flex flex-col pb-10 grow">
 	<div class="grid gap-4" style="grid-template-columns: repeat(auto-fill, minmax(216px, 1fr))">
 		<div class="flex flex-col border rounded bg-neutral-100 border-neutral-200 p-2.5">
 			<div
-				class="relative flex items-center justify-center w-full aspect-3/2 bg-neutral-200 rounded overflow-hidden"
+				class="relative flex items-center justify-center w-full overflow-hidden rounded aspect-3/2 bg-neutral-200"
 			>
 				<img
-					src="https://my-to-do.harryhdt.dev/apple-touch-icon-180x180.png"
+					src="/ooKit logo.webp"
 					class="absolute object-contain w-full h-full scale-90"
-					alt="My To Do"
+					alt="ooKit.dev"
 				/>
 				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
 			</div>
-			<div class="flex flex-col grow mt-2">
-				<h5 class="font-semibold text-neutral-800">My To Do</h5>
+			<div class="flex flex-col mt-2 grow">
+				<h5 class="font-semibold text-neutral-800">ooKit.dev</h5>
 				<p class="mb-2 text-sm">
-					is a Simple To Do App, with support cross platform. Write your To Do now.
+					A fast, modern, and reliable toolkit designed to help you get things done — effortlessly.
 				</p>
-				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
+				<div class="grid grid-cols-1 mt-auto text-center gap-x-1">
 					<a
 						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
 						target="_blank"
-						href="https://my-to-do.harryhdt.dev">Demo</a
-					>
-					<a
-						class="px-2.5 py-1.5 text-white transition-all duration-200 rounded bg-amber-600 hover:bg-amber-700/90 active:scale-95"
-						target="_blank"
-						href="https://github.com/harryhdt/My-To-Do">Source</a
+						href="https://ookit.pages.dev">Open</a
 					>
 				</div>
 			</div>
 		</div>
 		<div class="flex flex-col border rounded bg-neutral-100 border-neutral-200 p-2.5">
 			<div
-				class="relative flex items-center justify-center w-full aspect-3/2 bg-neutral-200 rounded overflow-hidden"
+				class="relative flex items-center justify-center w-full overflow-hidden rounded aspect-3/2 bg-neutral-200"
 			>
 				<img
-					src="https://cdn.harryhdt.dev/logo-link-preview.webp"
-					class="absolute object-contain w-full h-full scale-90"
-					alt="Link Preview"
-				/>
-				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
-			</div>
-			<div class="flex flex-col grow mt-2">
-				<h5 class="font-semibold text-neutral-800">Link Preview</h5>
-				<p class="mb-2 text-sm">
-					Get link preview like title, description, favicon, thumbnail and etc.
-				</p>
-				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
-					<a
-						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
-						target="_blank"
-						href="https://link-preview.harryhdt.dev">Demo</a
-					>
-					<a
-						class="px-2.5 py-1.5 text-white transition-all duration-200 rounded bg-amber-600 hover:bg-amber-700/90 active:scale-95"
-						target="_blank"
-						href="https://github.com/harryhdt/LinkPreview">Source</a
-					>
-				</div>
-			</div>
-		</div>
-		<div class="flex flex-col border rounded bg-neutral-100 border-neutral-200 p-2.5">
-			<div
-				class="relative flex items-center justify-center w-full aspect-3/2 bg-neutral-200 rounded overflow-hidden"
-			>
-				<img
-					src="https://cdn.harryhdt.dev/logo-bikin-qr.webp"
-					class="absolute object-contain w-full h-full scale-90"
-					alt="Link Preview"
-				/>
-				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
-			</div>
-			<div class="flex flex-col grow mt-2">
-				<h5 class="font-semibold text-neutral-800">Bikin QR</h5>
-				<p class="mb-2 text-sm">
-					Bikin QR Code untuk link atau teks kamu secara bebas dan gratis. Coba sekarang !!!
-				</p>
-				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
-					<a
-						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
-						target="_blank"
-						href="https://bikin-qr.harryhdt.dev">Demo</a
-					>
-					<a
-						class="px-2.5 py-1.5 text-white transition-all duration-200 rounded bg-amber-600 hover:bg-amber-700/90 active:scale-95"
-						target="_blank"
-						href="https://github.com/harryhdt/bikin-qr">Source</a
-					>
-				</div>
-			</div>
-		</div>
-		<div class="flex flex-col border rounded bg-neutral-100 border-neutral-200 p-2.5">
-			<div
-				class="relative flex items-center justify-center w-full aspect-3/2 bg-neutral-200 rounded overflow-hidden"
-			>
-				<img
-					src="https://cdn.harryhdt.dev/logo-classrooms-color.webp"
+					src="/logo-classrooms-color.webp"
 					class="absolute object-contain w-full h-full scale-90"
 					alt="Classrooms Color"
 				/>
 				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
 			</div>
-			<div class="flex flex-col grow mt-2">
+			<div class="flex flex-col mt-2 grow">
 				<h5 class="font-semibold text-neutral-800">Classrooms Color</h5>
-				<p class="mb-2 text-sm">
-					Buat kelas Anda yang penuh warna, nikmati suasana baru yang menyenangkan.
-				</p>
+				<p class="mb-2 text-sm">A frontend web that demonstrates the feature of coloring items.</p>
 				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
 					<a
 						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
