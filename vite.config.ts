7cbe2a7b47e29fa7d188c3c119d103f26b0c586f
Harry Hidayat <harryhdtofficial@gmail.com>
2026-07-10 19:02:45 +0800

    refactor: remove unused Svelte and Tailwind configurations; update Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			preprocess: vitePreprocess(),
+			adapter: adapter(),
+			output: { bundleStrategy: 'single' }
+		})
+	]
 });
