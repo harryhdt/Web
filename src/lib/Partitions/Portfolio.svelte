a2a741eb92ed6a685cf9d30ccd47f148dc1cfef5
Harry Hidayat <harryhdtofficial@gmail.com>
2026-07-11 08:53:51 +0800

    feat: update ooKit details and link in Portfolio component

--- src/lib/Partitions/Portfolio.svelte
+++ src/lib/Partitions/Portfolio.svelte
@@ -5,23 +5,23 @@
 				class="relative flex items-center justify-center w-full overflow-hidden rounded aspect-3/2 bg-neutral-200"
 			>
 				<img
-					src="/ooKit logo.webp"
+					src="https://ookit.harryhdt.dev/logo.svg"
 					class="absolute object-contain w-full h-full scale-90"
-					alt="ooKit.dev"
+					alt="ooKit"
 				/>
-				<!-- <IconImage class="w-16 h-16 opacity-50" /> -->
 			</div>
 			<div class="flex flex-col mt-2 grow">
-				<h5 class="font-semibold text-neutral-800">ooKit.dev</h5>
-				<p class="mb-2 text-sm">
-					A fast, modern, and reliable toolkit designed to help you get things done — effortlessly.
+				<h5 class="font-semibold text-neutral-800">ooKit</h5>
+				<p class="mb-2 text-sm line-clamp-2">
+					Free, fast, privacy-first online tools. Minifiers, converters, generators, and more —
+					zero tracking, we never store your data.
 				</p>
 				<div class="grid grid-cols-1 mt-auto text-center gap-x-1">
 					<a
 						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
 						target="_blank"
 						data-umami-event="Portfolio > ooKit > Open"
-						href="https://ookit.dev">Open</a
+						href="https://ookit.harryhdt.dev">Open</a
 					>
 				</div>
 			</div>
@@ -39,7 +39,9 @@
 			</div>
 			<div class="flex flex-col mt-2 grow">
 				<h5 class="font-semibold text-neutral-800">Classrooms Color</h5>
-				<p class="mb-2 text-sm">A frontend web that demonstrates the feature of coloring items.</p>
+				<p class="mb-2 text-sm line-clamp-2">
+					A frontend web that demonstrates the feature of coloring items.
+				</p>
 				<div class="grid grid-cols-2 mt-auto text-center gap-x-1">
 					<a
 						class="px-2.5 py-1.5 text-white transition-all duration-200 bg-blue-600 rounded hover:bg-blue-800/90 active:scale-95"
